f83ea85b172ad98cb0f35c638763b0d645d581d4
Universitario <rommeloscuro@gmail.com>
2018-09-20 19:09:06 -0400

    practica

---
+++ Practica 2-IA-2do_parcial/Assets/Scripts/Move.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public float speed = 2f;
+
+    public float maxSpeed = 5f;
+
+    private Rigidbody rb; 
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+    private void FixedUpdate()
+    {
+        Vector3 fixedVelocity = rb.velocity;
+        fixedVelocity.x *= 0.75f;
+
+        float h = Input.GetAxis("Horizontal");
+        rb.AddForce(Vector3.right * speed * h);
+        float limitedSpeed = Mathf.Clamp(rb.velocity.x, -maxSpeed, maxSpeed);
+        rb.velocity = new Vector3(limitedSpeed, rb.velocity.y);
+
+        if(h > 0.1f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        if (h < -0.1f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+
+    }
+}
